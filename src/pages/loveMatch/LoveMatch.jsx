a6506bce653780e275cfa54816a724c0941e0143
Jacopo Buffetti <jacopo.buffetti@thinkopen.it>
2020-09-01 00:24:13 +0200

    love match

--- src/pages/loveMatch/LoveMatch.jsx
+++ src/pages/loveMatch/LoveMatch.jsx
@@ -16,22 +16,62 @@ const LoveMatch = (props) => {
 
     const [inputFirstLoveData, setFirstInputLoveData] = useState('');
     const [inputSecondLoveData, setInputSecondLoveData] = useState('');
+    const [inputErrordLoveData, setInputErrorLoveData] = useState('');
+    const [parseNum, setParsNum] = useState('');
+
+    useEffect(() => {
+        setParsNum(parseInt(loveData.percentage))
+    }, [loveData]);
 
     const loveMatchSearch = () =>{
-        const love = {
-            inputFirstLoveData,
-            inputSecondLoveData
-        };
-        handleGetDataLove(love);
+        if(inputFirstLoveData.length > 1 && inputSecondLoveData.length > 1) {
+            const love = {
+                inputFirstLoveData,
+                inputSecondLoveData
+            };
+            handleGetDataLove(love);
+            setInputErrorLoveData('');
+        }else{
+            setInputErrorLoveData('Please check that both fields are correctly filled in');
+        }
     }
 
     return(
         <div className={`main-love main-page`}>
-            <input type="text"  placeholder={"First Name"} onChange={(e) => setFirstInputLoveData(e.target.value)} value={inputFirstLoveData}/>
-
-            <input type="text"  placeholder={"Second Name"} onChange={(e) => setInputSecondLoveData(e.target.value)} value={inputSecondLoveData} />
-
-            <button onClick={() => loveMatchSearch()}>CLICCAMI</button>
+            <div className={`full-page`}>
+                <div className={"back-love"} />
+            </div>
+            <div className={"container-love-card"}>
+                <div className={"card-love"}>
+                    <h1>Calculate love percentage</h1>
+                    <input type="text" id={'first-name'}  placeholder={"First Name"} onChange={(e) => setFirstInputLoveData(e.target.value)} value={inputFirstLoveData} required/>
+                    <input type="text"  placeholder={"Second Name"} onChange={(e) => setInputSecondLoveData(e.target.value)} value={inputSecondLoveData} required/>
+                    {inputErrordLoveData.length > 0 && (
+                        <h6>
+                            {inputErrordLoveData}
+                        </h6>
+                    )}
+                    <button onClick={() => loveMatchSearch()}>CLICCAMI</button>
+                    {typeof loveData.result !== "undefined" && (
+                        <div>
+                            <h3>{loveData.fname} - {loveData.sname}</h3>
+                            {parseNum > 50 ?
+                                <div className={"container-percentage"}>
+                                    <img className={"love-arrow"}
+                                         src={require('../../assets/image/love-heart-arrow.png')}/>
+                                    <p>{parseNum}%</p>
+                                </div> :
+                                <div className={"container-percentage"}>
+                                    <img className={"love-arrow"}
+                                         src={require('../../assets/image/broken-heart.png')}/>
+                                    <p>{parseNum}%</p>
+                                </div>
+                            }
+                            <h5>{loveData.result}</h5>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     )
 
